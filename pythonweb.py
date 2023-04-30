0670dc3ac7d09a90d5557bd951dc12ffb0c8dbef
Ajeet <57737420+yourajeet17@users.noreply.github.com>
2023-04-30 19:26:21 +0530

    not_done

--- pythonweb.py
+++ pythonweb.py
@@ -1,24 +1,17 @@
-#!C:\Users\ASUS\AppData\Local\Programs\Python\Python311
-print("content-Type:text/html")
-print()
+import mysql.connector
 import cgi
-
-print("<h1>Welcome to Python<?h1>")
-print("<hr/>")
-print("<h1>Using input tag</h1>")
-print("<body bgcolor='red'")
+con = mysql.connector.connect(host="localhost", user="root",password="",database="test")
 
 form=cgi.FieldStorage()
 id=form.getvalue("id")
 username=form.getvalue("username")
 email=form.getvalue("email")
 
-import mysql.connector
-con = mysql.connector.connect(host='localhost',user='root',password='',database="test")
-cur=con.cursor()
 
+
+cur=con.cursor()
 cur.execute("insert into test_tbl values(%s,%s,%s)",(id,username,email))
 con.commit()
 cur.close()
 con.close()
-print("<h3>Record Inserted successfully</h3>")
+print("Record Inserted successfully")
